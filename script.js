c8d995cd14e6aca1cacd06e46a2fd9d0ab7cfb74
Ivan <lambov97@mail.ru>
2021-05-20 19:37:16 +0200

    sravnenie

---
+++ script.js
@@ -0,0 +1,32 @@
+"use strict "
+if (4==7){
+    console.log ('ok!');
+}else {
+    console.log('eror ');
+}
+
+//if (num<49){
+  //  console.log('Error');
+//}else if(num>100){
+  //  console.log('mnojgo');
+//}else{
+  //  console.log('ok');
+//}
+//(num === 50) ? console.log('ok'): console.log('Error');
+const num = 50;
+
+switch(num){
+case 49:
+    console.log('neverno');
+    break;
+    case 100:
+        console.log('neverno');
+    break;
+     case 50:
+         console.log('verno');
+     break;
+default:
+    
+console.log('not now');
+break;
+}
